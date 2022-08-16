e5b603cc0f93aa87766b23c11667b2a69f9bc190
cengiz94 <sonsair11@gmail.com>
2022-08-16 13:07:59 +0300

    road lane and utils added

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ canvas.height=window.innerHeight;
 canvas.width=200;
 
 const context = canvas.getContext("2d");
-const road = new Road(canvas.width/2,canvas.width);
+const road = new Road(canvas.width/2,canvas.width*0.9);
 const car= new Car(100,100,30,50);
 car.draw(context);
 

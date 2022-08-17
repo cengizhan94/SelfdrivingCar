17a611da3023c1a0ca3310e71f3a05161e784c16
cengiz94 <sonsair11@gmail.com>
2022-08-17 09:57:35 +0300

    sensor fixed

--- js/car.js
+++ js/car.js
@@ -11,11 +11,13 @@ class Car{
         this.friction=0.05;
         this.angle=0;
 
+        this.sensor=new Sensor(this);
         this.controls = new Controls();
     }
 
     update(){
         this.#move();
+        this.sensor.update();
     }
     #move(){
         if(this.controls.forward){
@@ -54,7 +56,7 @@ class Car{
         this.y-=Math.cos(this.angle)*this.speed;
     }
 
-    draw(context){
+     draw(context){
         context.save();
         context.translate(this.x,this.y);
         context.rotate(-this.angle);
@@ -69,5 +71,7 @@ class Car{
         );
         context.fill();
         context.restore();
-    }
+
+        this.sensor.draw(context);
+    } 
 }
---
+++ js/sensor.js
@@ -0,0 +1,48 @@
+class Sensor{
+    constructor(car){
+        this.car = car;
+        this.rayCount=3;
+        this.rayLength=100;
+        this.raySpread=Math.PI/4
+
+        this.rays=[];
+    }
+
+    update(){
+        this.#castRays();
+    }
+    #castRays(){
+        this.rays=[];
+        for(let i=0; i<this.rayCount; i++){
+            const rayAngle=lerp(
+                this.raySpread/2,
+                -this.raySpread/2,
+                this.rayCount==1?0.5:i/(this.rayCount-1)
+            )+this.car.angle;
+
+            const start={x:this.car.x, y:this.car.y};
+            const end={
+                x:this.car.x-Math.sin(rayAngle)*this.rayLength,
+                y:this.car.y-Math.cos(rayAngle)*this.rayLength
+            };
+            this.rays.push([start,end]);
+        }
+    }
+
+     draw(context){
+        for(let i=0;i<this.rayCount; i++){
+            context.beginPath();
+            context.lineWidth=2;
+            context.strokeStyle="yellow";
+            context.moveTo(
+                this.rays[i][0].x,
+                this.rays[i][0].y
+            );
+            context.lineTo(
+               this.rays[i][1].x,
+               this.rays[i][1].y 
+            );
+            context.stroke();
+        }
+    } 
+}
